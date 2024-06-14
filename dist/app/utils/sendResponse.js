043648225800b77f927f823e42802daef7cef643
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-14 11:34:33 +0600

    implement sendResponse util

--- dist/app/utils/sendResponse.js
+++ dist/app/utils/sendResponse.js
@@ -4,6 +4,7 @@ const sendResponse = (res, data) => {
     const { statusCode, message } = data;
     return res.status(statusCode).json({
         success: statusCode >= 200 && statusCode < 400,
+        statusCode,
         message,
         data: data.data,
     });
---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,20 @@
+import { Response } from 'express';
+
+interface ResponseData<T> {
+    statusCode: number;
+    message: string;
+    data: T;
+}
+
+const sendResponse = <T>(res: Response, data: ResponseData<T>) => {
+    const { statusCode, message } = data;
+
+    return res.status(statusCode).json({
+        success: statusCode >= 200 && statusCode < 400,
+        statusCode,
+        message,
+        data: data.data,
+    });
+};
+
+export default sendResponse;
